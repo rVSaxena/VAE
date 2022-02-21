aa42c0d3468db8a215766f96534832b4e78194f7
Racing Panda <44290569+rVSaxena@users.noreply.github.com>
2022-02-22 02:36:59 +0530

    fixed dtype inconsistensies

--- constructs.py
+++ constructs.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import pandas as pd
 from resnet import ResidualBlock
 
 class Reshape(nn.Module):
@@ -12,15 +13,32 @@ class Reshape(nn.Module):
 
     def forward(self, x):
 
-        return x.view(self.out_shape)
+        return x.reshape(self.out_shape)
+
+class mnist_dataset(torch.utils.data.Dataset):
+
+    def __init__(self):
+        return
+
+    def __len__(self):
+        return 70000
+
+    def __getitem__(self, index):
+        x=torch.from_numpy((pd.read_csv("data/mnist.csv", skiprows=index, nrows=1).values[0, 1:].astype('double')/255).reshape((1, 28, 28)))
+        return x
 
 
 args={}
+args["device"]="cuda"
+
+dataset=mnist_dataset()
+args["trainLoader"]=torch.utils.data.DataLoader(dataset, batch_size=64, shuffle=True, num_workers=0)
+
 args["epochs"]=50
 args["data_dim"]=28*28
 args["latent_dim"]=4
-args["num_expectation_samples"]=200
-args["logging_dir"]
+args["num_expectation_samples"]=5
+args["logging_dir"]="mnist_run/"
 
 # made sure encoder returns (batch_size, 2*latent_dim)
 encoder=nn.Sequential(
@@ -31,7 +49,7 @@ encoder=nn.Sequential(
     ResidualBlock(4, 1, True, 3),
     nn.Flatten(start_dim=1, end_dim=-1),
     nn.Linear(49, 8)
-    )
+    ).double().to(args["device"])
 
 # decoder will be given (batch_size, num_expectation_samples, latent_dim)
 
@@ -44,12 +62,12 @@ decoder=nn.Sequential(
     nn.Linear(64, 784),
     nn.Sigmoid(),
     Reshape((-1, 1, 28, 28))
-    )
+    ).double().to(args["device"])
 
 args["encoder_net"]=encoder
 args["decoder_net"]=decoder
 
 params=list(args["encoder_net"].parameters())+list(args["decoder_net"].parameters())
-args["optimizer"]=torch.optim.Adam(params)
+args["optimizers"]=[torch.optim.Adam(params)]
 
-args["lr_scheduler"]=torch.optim.lr_scheduler.StepLR(optimizer, 10, gamma=0.1)
+args["lr_schedulers"]=[torch.optim.lr_scheduler.StepLR(args["optimizers"][0], 10, gamma=0.1)]
