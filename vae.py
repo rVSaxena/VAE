2ec81489ea42c748d07f952f7cfa1b357dcb9b76
Racing Panda <44290569+rVSaxena@users.noreply.github.com>
2022-02-21 14:46:23 +0530

    first commit, only VAE implemented

---
+++ vae.py
@@ -0,0 +1,40 @@
+import torch
+from torch import nn
+
+class VAE(nn.Module):
+
+	def __init__(self, data_dimension, latent_dimension, encoder_net, decoder_net, num_expectation_samples=500):
+
+		"""
+		Decoder should expect an input of shape (num_x, num_exepectation_samples, latent_dimension)
+		"""
+
+		super(VAE, self).__init__()
+		self.data_dim=data_dimension
+		self.latent_dim=latent_dimension
+		self.encoder=encoder_net
+		self.decoder=decoder
+		self.num_expectation_samples=num_expectation_samples
+
+		return
+
+	def forward(self, x):
+		# The shape of x is supposed to be 
+		# (N, data_dim)
+
+		g_stats=self.encoder(x)
+		# Expected shape is (N, 2*latent_dimension) -> mean and the diag element of the cov_mat
+		# g_stats contains the numbers needed to construct the mean and cov matrix,
+		# for use in re-parameterization. cov mat is chosen to be diagonal
+
+		mean, cov=g_stats[:, :self.latent_dimension], torch.exp(torch.diag_embed(g_stats[:, self.latent_dimension:].reshape((-1, self.latent_dim, self.latent_dim))))
+		
+		with torch.no_grad():
+			epsilons=torch.normal(0, 1, size=(mean.shape[0], self.num_expectation_samples, self.latent_dim))
+			all_z=(cov@epsilons.swapaxes(1, 2)).swapaxes(1, 2)+mean[:, None, :]
+
+		output=self.decoder(all_z)
+
+		return output, mean, cov
+
+
