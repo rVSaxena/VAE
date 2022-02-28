389d1d4a70e60958d190d1bf81de64ef91e9e42b
Racing Panda <44290569+rVSaxena@users.noreply.github.com>
2022-02-28 12:08:19 +0530

    different constructs

--- constructs.py
+++ constructs.py
@@ -18,56 +18,75 @@ class Reshape(nn.Module):
 class mnist_dataset(torch.utils.data.Dataset):
 
     def __init__(self):
+        self.all_data=torch.from_numpy((pd.read_csv("data/mnist.csv").values[:, 1:].astype('double')/255).reshape((-1, 1, 28, 28)))
         return
 
     def __len__(self):
-        return 70000
+        return len(self.all_data)
 
     def __getitem__(self, index):
-        x=torch.from_numpy((pd.read_csv("data/mnist.csv", skiprows=index, nrows=1).values[0, 1:].astype('double')/255).reshape((1, 28, 28)))
-        return x
+        return self.all_data[index]
 
 
 args={}
 args["device"]="cuda"
 
 dataset=mnist_dataset()
-args["trainLoader"]=torch.utils.data.DataLoader(dataset, batch_size=64, shuffle=True, num_workers=0)
+args["trainLoader"]=torch.utils.data.DataLoader(dataset, batch_size=64, shuffle=True)
 
-args["epochs"]=50
+args["epochs"]=80
 args["data_dim"]=28*28
-args["latent_dim"]=4
-args["num_expectation_samples"]=5
+args["latent_dim"]=3
+args["num_expectation_samples"]=20
 args["logging_dir"]="mnist_run/"
 
 # made sure encoder returns (batch_size, 2*latent_dim)
 encoder=nn.Sequential(
-    ResidualBlock(1, 64, False, 3),
-    ResidualBlock(64, 64, False, 3),
-    ResidualBlock(64, 32, False, 3),
-    ResidualBlock(32, 4, True, 3),
+    ResidualBlock(1, 8, False, 3),
+    ResidualBlock(8, 16, False, 3),
+    ResidualBlock(16, 16, False, 3),
+    ResidualBlock(16, 4, True, 3),
     ResidualBlock(4, 1, True, 3),
     nn.Flatten(start_dim=1, end_dim=-1),
-    nn.Linear(49, 8)
+    nn.Linear(49, 2*args["latent_dim"])
     ).double().to(args["device"])
+
+# encoder=nn.Sequential(
+#     nn.Linear(784, 16),
+#     nn.Sigmoid(),
+#     nn.Linear(16, 16),
+#     nn.Sigmoid(),
+#     nn.Linear(16, 2*args["latent_dim"])
+#     ).double().to(args["device"])
 
 # decoder will be given (batch_size, num_expectation_samples, latent_dim)
 
 decoder=nn.Sequential(
     Reshape((-1, args["latent_dim"])),
-    nn.Linear(args["latent_dim"], 64),
+    nn.Linear(args["latent_dim"], 16),
     nn.ReLU(inplace=True),
-    nn.Linear(64, 64),
+    nn.Linear(16, 16), 
     nn.ReLU(inplace=True),
-    nn.Linear(64, 784),
-    nn.Sigmoid(),
-    Reshape((-1, 1, 28, 28))
+    nn.Linear(16, 784),
+    nn.ReLU(inplace=True),
+    Reshape((-1, args["num_expectation_samples"], 1, 28, 28))
     ).double().to(args["device"])
 
 args["encoder_net"]=encoder
 args["decoder_net"]=decoder
 
 params=list(args["encoder_net"].parameters())+list(args["decoder_net"].parameters())
-args["optimizers"]=[torch.optim.Adam(params)]
+args["optimizers"]=[torch.optim.Adam(params, lr=1e-3)]
 
-args["lr_schedulers"]=[torch.optim.lr_scheduler.StepLR(args["optimizers"][0], 10, gamma=0.1)]
+def f1(epoch):
+    if epoch>12:
+        return 1.0
+    if epoch%3==0:
+        return 0.6**(epoch/3)
+    return 1
+
+
+args["lr_schedulers"]=[
+torch.optim.lr_scheduler.StepLR(args["optimizers"][0], 9, gamma=0.6)
+# torch.optim.lr_scheduler.LambdaLR(args["optimizers"][0], lr_lambda=f1)
+]
